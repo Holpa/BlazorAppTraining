89416e74d689010f6a9ba4d213c2dce17e4b7b76
Ahmad Holpa <ahmadholpa@gmail.com>
2024-03-23 02:55:46 -0400

    Second commit all the files

---
+++ BlazorAppTraining/Program.cs
@@ -0,0 +1,35 @@
+using BlazorAppTraining.Client.Pages;
+using BlazorAppTraining.Components;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorComponents()
+    .AddInteractiveServerComponents()
+    .AddInteractiveWebAssemblyComponents();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseWebAssemblyDebugging();
+}
+else
+{
+    app.UseExceptionHandler("/Error", createScopeForErrors: true);
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+
+app.UseStaticFiles();
+app.UseAntiforgery();
+
+app.MapRazorComponents<App>()
+    .AddInteractiveWebAssemblyRenderMode()
+    .AddInteractiveServerRenderMode()
+    .AddAdditionalAssemblies(typeof(BlazorAppTraining.Client._Imports).Assembly);
+
+app.Run();
